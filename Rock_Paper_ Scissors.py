4b658fe2c5d9455ec2765ea8758038c0962cf498
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2019-07-29 13:29:37 +0700

    Add score counting

--- Rock_Paper_ Scissors.py
+++ Rock_Paper_ Scissors.py
@@ -1,6 +1,9 @@
 import random
 
 start_game = 'p'
+player_score = 0
+com_score = 0
+
 while start_game == 'p':
     input_text = input("Rock (r), Paper (p) ,Scissor (s)").lower()
     while input_text not in ('r','p','s'):
@@ -21,28 +24,45 @@ while start_game == 'p':
 #####################################################################################################################
 # Game Result
 #####################################################################################################################
+
     if input_text == com:
         print(input_text,'VS',com)
         print('Draw!!')
+        print('Player: ',player_score,' ','Com: ',com_score)
     elif input_text =='p' and com == 's':
         print(input_text,'VS',com)
+        com_score = com_score +1
         print('You Lose')
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
     elif input_text =='r' and com == 'p':
         print(input_text,'VS',com)
+        com_score = com_score +1
         print('You Lose')
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
     elif input_text =='s' and com == 'r':
         print(input_text,'VS',com)
+        com_score = com_score +1
         print('You Lose')
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
     elif input_text =='s' and com == 'p':
         print(input_text,'VS',com)
         print('You Win')
+        player_score = player_score +1
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
     elif input_text =='p' and com == 'r':
         print(input_text,'VS',com)
         print('You Win')
+        player_score = player_score +1
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
     elif input_text =='r' and com == 's':
         print(input_text,'VS',com)
         print('You Win')
+        player_score = player_score +1
+        print('Player: ', player_score, ' ', 'Com: ', com_score)
+
     start_game = input('Play again (P) or Quit (Q): ').lower()
+    if start_game == 'q':
+        print('Good Bye!')
 
 
         
