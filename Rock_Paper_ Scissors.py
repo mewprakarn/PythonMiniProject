44f3ffa2a2a3abda74e2fee8a43b634749663684
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2019-07-29 13:12:21 +0700

    Add Continue or Quit game option
    Input while loop at top line

--- Rock_Paper_ Scissors.py
+++ Rock_Paper_ Scissors.py
@@ -1,45 +1,49 @@
 import random
-input_text = input("Rock (r), Paper (p) ,Scissor (s)")
-while input_text not in ('r','p','s'):
-    print('Wrong input')
+
+start_game = 'p'
+while start_game == 'p':
     input_text = input("Rock (r), Paper (p) ,Scissor (s)")
+    while input_text not in ('r','p','s'):
+        print('Wrong input')
+        input_text = input("Rock (r), Paper (p) ,Scissor (s)")
 
 #####################################################################################################################
 # Computer turn
 #####################################################################################################################
-rand = random.randint(1,3)
-if rand == 1:
-    com = 'p'
-elif rand == 2:
-    com = 'r'
-else:
-    com ='s'
+    rand = random.randint(1,3)
+    if rand == 1:
+        com = 'p'
+    elif rand == 2:
+        com = 'r'
+    else:
+        com ='s'
 
 #####################################################################################################################
 # Game Result
 #####################################################################################################################
-if input_text == com:
-    print(input_text,'VS',com)
-    print('Draw!!')
-elif input_text =='p' and com == 's':
-    print(input_text,'VS',com)
-    print('You Lose')
-elif input_text =='r' and com == 'p':
-    print(input_text,'VS',com)
-    print('You Lose')
-elif input_text =='s' and com == 'r':
-    print(input_text,'VS',com)
-    print('You Lose')
-elif input_text =='s' and com == 'p':
-    print(input_text,'VS',com)
-    print('You Win')
-elif input_text =='p' and com == 'r':
-    print(input_text,'VS',com)
-    print('You Win')
-elif input_text =='r' and com == 's':
-    print(input_text,'VS',com)
-    print('You Win')
-            
+    if input_text == com:
+        print(input_text,'VS',com)
+        print('Draw!!')
+    elif input_text =='p' and com == 's':
+        print(input_text,'VS',com)
+        print('You Lose')
+    elif input_text =='r' and com == 'p':
+        print(input_text,'VS',com)
+        print('You Lose')
+    elif input_text =='s' and com == 'r':
+        print(input_text,'VS',com)
+        print('You Lose')
+    elif input_text =='s' and com == 'p':
+        print(input_text,'VS',com)
+        print('You Win')
+    elif input_text =='p' and com == 'r':
+        print(input_text,'VS',com)
+        print('You Win')
+    elif input_text =='r' and com == 's':
+        print(input_text,'VS',com)
+        print('You Win')
+    start_game = input('Play again (P) or Quit (Q): ').lower()
+
 
         
 
