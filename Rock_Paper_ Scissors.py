cae49bb8ad62cd551656a2fbc7857df1acb88951
mewprakarn <51776670+mewprakarn@users.noreply.github.com>
2019-07-29 10:19:26 +0700

    Rock_Paper_Scissor Game
    Initial code including game rule and how AI choose its move.

---
+++ Rock_Paper_ Scissors.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+
+
+# In[103]:
+
+
+input_text = input("Rock (r), Paper (p) ,Scissor (s)")
+while input_text not in ('r','p','s'):
+    print('Wrong input')
+    input_text = input("Rock (r), Paper (p) ,Scissor (s)")
+    
+# Computer turn
+rand = random.randint(1,3)
+if rand == 1:
+    com = 'p'
+elif rand == 2:
+    com = 'r'
+else:
+    com ='s'
+
+# Result check
+if input_text == com:
+    print(input_text,'VS',com)
+    print('Draw!!')
+elif input_text =='p' and com == 's':
+    print(input_text,'VS',com)
+    print('You Lose')
+elif input_text =='r' and com == 'p':
+    print(input_text,'VS',com)
+    print('You Lose')
+elif input_text =='s' and com == 'r':
+    print(input_text,'VS',com)
+    print('You Lose')
+elif input_text =='s' and com == 'p':
+    print(input_text,'VS',com)
+    print('You Win')
+elif input_text =='p' and com == 'r':
+    print(input_text,'VS',com)
+    print('You Win')
+elif input_text =='r' and com == 's':
+    print(input_text,'VS',com)
+    print('You Win')
+            
+
+        
+
